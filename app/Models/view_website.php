7eef86d5bcb8968ecb9ceec0e9066fe1437d4cd2
NurFazdlin <syakiena12@gmail.com>
2023-12-14 19:53:49 +0800

    first commit

---
+++ app/Models/view_website.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class view_website extends Model
+{
+    use HasFactory;
+}
