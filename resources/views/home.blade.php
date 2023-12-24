d9617ecc39192e5207d4fdec3427aa12e7916571
nureesazd31 <nureesa.zd@gmail.com>
2023-12-25 00:11:43 +0800

    tukar h jadik x

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashhhhhhhhhhhhhboard') }}</div>
+                <div class="card-header">{{ __('Dasxxxboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
