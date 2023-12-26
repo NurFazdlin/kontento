673b1c8a78030a144e6ad1fc635621b675731138
NurFazdlin <syakiena12@gmail.com>
2023-12-27 00:54:44 +0800

    update room availabilities

--- database/migrations/2023_12_14_102242_create_room_availabilities_table.php
+++ database/migrations/2023_12_14_102242_create_room_availabilities_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('roomnumber')->unique();
             $table->enum('typeofhomestay', ['kontena','villaredan','glamping']);
-            $table->string('roomtype');
             $table->integer('occupancy');
             $table->text('description')->nullable();
             $table->string('price');
